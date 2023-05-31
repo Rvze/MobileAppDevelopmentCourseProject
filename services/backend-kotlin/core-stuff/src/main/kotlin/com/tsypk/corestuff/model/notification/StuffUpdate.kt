f271f65adff64e6397e7d160b5a88617af707b60
flyfalldie <kolya_cypandin@mail.ru>
2023-06-01 02:14:09 +0300

    stuff update refactor

--- services/backend-kotlin/core-stuff/src/main/kotlin/com/tsypk/corestuff/model/notification/StuffUpdate.kt
+++ services/backend-kotlin/core-stuff/src/main/kotlin/com/tsypk/corestuff/model/notification/StuffUpdate.kt
@@ -8,11 +8,18 @@ enum class StuffUpdateType {
     DELETE,
 }
 
+data class PriceUpdate(
+        @JsonProperty("old_price")
+        val oldPrice: Long,
+        @JsonProperty("new_price")
+        val newPrice: Long,
+)
+
 data class StuffUpdate(
-    @JsonProperty("type")
-    val type: StuffUpdateType,
-    @JsonProperty("model_id")
-    val modelId: String,
-    @JsonProperty("price")
-    val price: Long?
+        @JsonProperty("type")
+        val type: StuffUpdateType,
+        @JsonProperty("model_id")
+        val modelId: String,
+        @JsonProperty("payload")
+        val payload: PriceUpdate?,
 )
